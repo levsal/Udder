da7b82a6d9004aff9de29a5dc86ebef17a7e4644
Levi Saltzman <levisaltzman@gmail.com>
2020-06-29 16:08:53 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Levi Saltzman on 6/29/20.
+//  Copyright © 2020 Levi Saltzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
